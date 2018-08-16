4befdd4bd3eba31706beeb76af45b1a10ad31e3c
syahrulutomo <irullvandalis@gmail.com>
2018-08-17 06:00:31 +0700

    fix controller

--- src/main/java/com/ttscaklontong/backendtts/controller/QuizController.java
+++ src/main/java/com/ttscaklontong/backendtts/controller/QuizController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -32,15 +33,28 @@ public class QuizController {
     private QuizRepository qr;
     
     @GetMapping(path="/view")
-    public @ResponseBody Iterable<Quiz> getAllUsers() {
+    public @ResponseBody Iterable<Quiz> getAllQuestion() {
         // This returns a JSON or XML with the users
 	return qr.findAll();
     }
     
     @RequestMapping(value="/add", method = RequestMethod.POST)
      @ResponseStatus(HttpStatus.CREATED)
-     public void insertUserBaru(@RequestBody Quiz q){
+     public void insertNewQuestion(@RequestBody Quiz q){
          qr.save(q);
+     }
+     
+     @RequestMapping(value="/question/{id}", method = RequestMethod.PUT)
+     @ResponseStatus(HttpStatus.OK)
+     public void updateQuestion(@PathVariable("id") Integer id, @RequestBody Quiz q){
+         q.setId(id);
+         qr.save(q);
+     }
+     
+     @RequestMapping(value="/question/{id}", method = RequestMethod.DELETE)
+     @ResponseStatus(HttpStatus.OK)
+     public void hapusUser(@PathVariable("id") Long id){
+         qr.deleteById(id);
      }
     
 }
