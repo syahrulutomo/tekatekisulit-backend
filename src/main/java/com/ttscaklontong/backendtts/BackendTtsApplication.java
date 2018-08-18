5780450b61eab2497d10186bf3bf94fde7d8ff20
syahrulutomo <irullvandalis@gmail.com>
2018-08-18 17:05:40 +0700

    add corsfilter

--- src/main/java/com/ttscaklontong/backendtts/BackendTtsApplication.java
+++ src/main/java/com/ttscaklontong/backendtts/BackendTtsApplication.java
@@ -2,6 +2,7 @@ package com.ttscaklontong.backendtts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +12,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class BackendTtsApplication {
 
     public static void main(String[] args) {
-	SpringApplication.run(BackendTtsApplication.class, args);
-	}
+	SpringApplication.run(BackendTtsApplication.class, args);	
+    }
+    
+     @Bean
+    public FilterRegistrationBean corsFilterRegistration() {
+        FilterRegistrationBean registrationBean =
+                new FilterRegistrationBean(new CORSFilter());
+        registrationBean.setName("CORS Filter");
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(1);
+        return registrationBean;
+    }
 }
